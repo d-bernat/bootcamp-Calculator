123e1172ee8b7ca3c9d3430a8e3883e86c1172b4
bernat <bernat@traveltainment.de>
2017-11-27 19:22:56 +0100

    another init

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,19 +10,43 @@ namespace bootcamp
     {
         static void Main(string[] args)
         {
-            SimpleCalculator calc = new SimpleCalculator();
-            String result = calc.add(3, 4) + "\n";
+            ICalculator calc = new SciCalculator();
+            String result = calc.substr(3, 4) + "\n";
             Console.Write(result);
             Console.Read();
         }
     }
 
 
-    class SimpleCalculator
+    class SimpleCalculator : ICalculator
     {
         public long add(int x, int y)
         {
             return x + y;
         }
+
+        public long substr(int x, int y)
+        {
+            return x - y;
+        }
+    }
+
+    class SciCalculator : ICalculator
+    {
+        public long add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public long substr(int x, int y)
+        {
+            return x - y;
+        }
+    }
+
+    interface ICalculator
+    {
+        long add(int x, int y);
+        long substr(int x, int y);
     }
 }
