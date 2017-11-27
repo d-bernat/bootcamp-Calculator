483cf8f96a52049cf7a63a8805f71c63a001edd6
bernat <bernat@traveltainment.de>
2017-11-27 22:17:26 +0100

    try it

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,11 +8,33 @@ namespace bootcamp
 {
     class Calculator
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             ICalculator calc = new SciCalculator();
-            String result = calc.substr(3, 4) + "\n";
+            string result = calc.substr(3, 4) + "\n";
+
             Console.Write(result);
+            foreach(string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            try
+            {
+                double.Parse("123.452");
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+            Console.WriteLine(Environment.UserName);
+
+            Console.WriteLine(calc.getDateTime().Date);
+
+
+
             Console.Read();
         }
     }
@@ -25,9 +47,19 @@ namespace bootcamp
             return x + y;
         }
 
-        public long substr(int x, int y)
+        public DateTime getDateTime()
         {
-            return x - y;
+            throw new NotImplementedException();
+        }
+
+        public TimeSpan getDateTimeSpan()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong substr(int x, int y)
+        {
+            return (ulong)(x - y);
         }
     }
 
@@ -38,15 +70,48 @@ namespace bootcamp
             return x + y;
         }
 
-        public long substr(int x, int y)
+        public DateTime getDateTime()
         {
-            return x - y;
+            return DateTime.Now;
+        }
+
+        public TimeSpan getDateTimeSpan()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong substr(int x, int y)
+        {
+            IList<String> l = new List<String>();
+            l.Add("ahoj");
+            l.Add("vole");
+
+            foreach (String s in l)
+            {
+                Console.WriteLine(s);
+
+            }
+
+            foreach(string s in Environment.GetCommandLineArgs())
+                Console.WriteLine(s);
+            unchecked
+            {
+                return (ulong)(x - y);
+            }
         }
     }
 
-    interface ICalculator
+    interface ICalculator : IDateTimeable
     {
         long add(int x, int y);
-        long substr(int x, int y);
+        ulong substr(int x, int y);
     }
+
+    interface IDateTimeable
+    {
+        DateTime getDateTime();
+        TimeSpan getDateTimeSpan();
+
+    }
+
 }
